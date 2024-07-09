4a0b746c216778fe5cb975cf87e011e8ccca2354
Sean <sean.helwig@gmail.com>
2024-07-09 15:46:29 -0400

    maybe this time

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT
 
+app.use(express.json())
+
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.get("/test", (req, res) => res.send("Test route works, so what the fuck?"))
