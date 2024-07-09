e4c138895373479c898ee07af1fd834bd0495f85
Sean <sean.helwig@gmail.com>
2024-07-09 15:45:42 -0400

    fixed port maybe

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.get("/test", (req, res) => res.send("Test route works, so what the fuck?"))
+
+app.listen(port, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
